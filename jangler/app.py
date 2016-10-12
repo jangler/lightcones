db3754d84b952226354450c9fc8195fbfde039e9
Brandon Mulcahy <brandon@jangler.info>
2016-10-12 12:38:58 -0400

    Serve a semi-proper index page

--- jangler/app.py
+++ jangler/app.py
@@ -1,11 +1,20 @@
+import pathlib
+
 import flask
 
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'hello, world!'
+def index():
+    return flask.render_template('index.html', title='news')
+
+
+# generate URLs for static files
+with app.test_request_context():
+    for p in pathlib.Path('static').iterdir():
+        flask.url_for('static', filename=str(p))
 
 
 if __name__ == '__main__':
